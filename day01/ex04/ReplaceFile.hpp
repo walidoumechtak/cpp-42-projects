689103e8f93e62a1d6564e73705cfe48bc2befc1
Walid Oumechtak <woumecht@e1r3p3.1337.ma>
2023-05-10 11:42:54 +0100

    almost done with exe04

--- day01/ex04/ReplaceFile.hpp
+++ day01/ex04/ReplaceFile.hpp
@@ -1,12 +1,20 @@
+#ifndef REPLACEFILE_HPP
+#define REPLACEFILE_HPP
+
 #include <iostream>
 #include <string>
+#include <fstream>
 
 class ReplaceFile{
 
 private:
     std::string fileName;
+    std::string newFileName;
     std::string s1;
     std::string s2;
+    std::fstream oFile;
+    std::fstream nFile;
+
 
 public:
     ReplaceFile();
@@ -18,5 +26,10 @@ public:
     void    setFileName(std::string fn);
     void    setS1(std::string ss1);
     void    setS2(std::string ss2);
-    void    open_file(std::string fileN);
-}
+    void    open_file(std::string fileN, std::string flag);
+    void    close_files();
+    void    ft_replace();
+
+};
+
+#endif
