f515322e1940fb29dd63f6b44ac99a9669693a73
Walid Oumechtak <woumecht@c3r2p9.1337.ma>
2023-09-02 15:56:30 +0100

    start the day 5 and complete the first exercice

---
+++ day05/ex00/Bureaucrat.hpp
@@ -0,0 +1,63 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+#include <iostream>
+#include <exception>
+#include <sstream>
+
+
+class Bureaucrat
+{
+    public:
+        class GradeTooHighException : public std::exception
+            {
+                private:
+                    std::string msg;
+                public:
+                    GradeTooHighException(std::string in_msg);
+                    ~GradeTooHighException() throw();
+                    virtual const char* what() const throw();
+            };
+
+            class GradeTooLowException : public std::exception
+            {
+                private:
+                    std::string msg;
+                public:
+                    GradeTooLowException(std::string in_msg);
+                    ~GradeTooLowException() throw();
+                    virtual const char* what() const throw();
+            };
+
+            class GradeOutOfRange : public std::exception
+            {
+                private:
+                    std::string msg;
+                public:
+                    GradeOutOfRange(std::string in_msg);
+                    ~GradeOutOfRange() throw();
+                    virtual const char* what() const throw();
+            };
+
+    private:
+        const std::string name;
+        int grade;
+    public:
+        Bureaucrat();
+        Bureaucrat(const std::string in_name, int in_grade);
+        Bureaucrat(const Bureaucrat& source);
+        Bureaucrat& operator= (const Bureaucrat& source);
+        ~Bureaucrat();
+        int getGrade() const;
+        std::string getName() const;
+        void    incGrade();
+        void    decGrade();
+
+       
+
+};
+
+std::ostream& operator<<(std::ostream & os, const Bureaucrat& obj);
+
+#endif
---
+++ day05/ex00/main.cpp
@@ -0,0 +1,56 @@
+#include "Bureaucrat.hpp"
+
+int main(void)
+{
+    try
+    {
+        Bureaucrat b1("walid", 5);
+        std::cout << b1 << std::endl;
+        b1.incGrade();
+        b1.incGrade();
+        std:: cout << b1 << std::endl;
+        b1.decGrade();
+        std:: cout << b1 << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    std:: cout << "__________________________" << std::endl << std::endl;
+
+    try
+    {
+        Bureaucrat b2("Low", 200);
+        std::cout << b2 << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+        
+    std:: cout << "__________________________" << std::endl << std::endl;
+
+    try
+    {
+        Bureaucrat b3("Height", 0);
+        std::cout << b3 << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    std:: cout << "__________________________" << std::endl << std::endl;
+    try
+    {
+        Bureaucrat b4("OutRange", 2);
+        b4.incGrade();
+        b4.incGrade();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    
+}
