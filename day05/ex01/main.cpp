9c8e82a799abd4172df040a77d8829f804c38eb7
Walid Oumechtak <woumecht@c3r2p9.1337.ma>
2023-09-04 11:54:59 +0100

    done with ex 01

--- day05/ex01/main.cpp
+++ day05/ex01/main.cpp
@@ -6,12 +6,12 @@ int main(void)
     try
     {
         Bureaucrat b1("walid", 5);
-        Form f1("Certificat", 10, 55);
+        Form f1("Cer1", 10, 55);
         std::cout << f1 << std::endl;
         b1.signForm(f1);
         f1.beSigned(b1);
-        std::cout << f1 << std::endl;
         b1.signForm(f1);
+        std::cout << f1 << std::endl;
     }
     catch(const std::exception& e)
     {
@@ -22,14 +22,27 @@ int main(void)
     
     try
     {
+        // in this case the form has a grad to sign to heigh so an exception occure
         Bureaucrat b2("med", 10);
-        Form f1("Off Form", 0, 100);
-        f1.beSigned(b2);
+        Form f2("Cer2", 0, 100);
+        f2.beSigned(b2);
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << std::endl;
     }
 
-    
+    std:: cout << "__________________________" << std::endl << std::endl;
+
+    try
+    {
+        // in this case the bureaucrat have a grad too low to sign the form so an exception occure
+        Bureaucrat b2("med", 10);
+        Form f2("Cer2", 1, 100);
+        f2.beSigned(b2);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
 }
