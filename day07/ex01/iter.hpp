ef7259c0cc6345f889109b6dc4a48ba114b6dd30
Walid Oumechtak <woumecht@e1r2p11.1337.ma>
2023-10-13 15:29:38 +0100

    done with 7

--- day07/ex01/iter.hpp
+++ day07/ex01/iter.hpp
@@ -1,8 +1,9 @@
 #ifndef ITER_HPP
 #define ITER_HPP
+#include <iostream>
 
-template <typename T1, typename T2, typename T3>
-void iter(T1 *arr, int len, T2 (*f)(T3 &ele))
+template <typename T1, typename Func, typename T3>
+void iter(T1 *arr, int len, Func (*f)(T3 &ele))
 {
     for (int i = 0; i < len; i++)
     {
@@ -10,5 +11,10 @@ void iter(T1 *arr, int len, T2 (*f)(T3 &ele))
     }
 }
 
+template <typename RET, typename T1>
+RET TODO(T1 &t)
+{
+    t++;
+}
 
 #endif
--- day07/ex01/main.cpp
+++ day07/ex01/main.cpp
@@ -2,18 +2,42 @@
 #include <iostream>
 #include <string>
 
-
-void    increment(int &a)
+class Complex
 {
-    a++;
+    private:
+        int n;
+    public:
+        Complex() : n(42) {}
+        Complex(int _n) : n(_n) {}
+        int getInt() const { return n; }
+};
+
+std::ostream &operator << (std::ostream &o, const Complex& obj)
+{
+    o << obj.getInt();
+    return o;
+}
+
+template <typename T>
+void print(T const& item)
+{
+    std::cout << item << std::endl;
 }
 
 int main(void)
 {
-    int size = 5;
-    int arr[5] = {0};
+    // int size = 5;
+    // int arr[5] = {0};
 
-    iter(arr, size, &increment);
-    for (int i = 0; i < size; i++)
-        std::cout << arr[i] << std::endl;
+    // iter(arr, size, TODO<void, int>);
+    // for (int i = 0; i < size; i++)
+    //     std::cout << arr[i] << std::endl;
+
+    // ===================================================
+
+    Complex obj[5];
+    int arr[5] = {0,1,2,3,4};
+
+    iter(obj, 5, print<Complex>);
+    iter(arr, 5, print<int>);
 }
