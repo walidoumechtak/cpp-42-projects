c69c580a4fddf1bd61026fde39d35e9542b57ebe
walid oumechtak <woumechtak@gmail.com>
2023-12-08 22:35:30 +0100

    add some modification

--- day09/ex01/RPN.cpp
+++ day09/ex01/RPN.cpp
@@ -11,10 +11,61 @@ bool parseTheExpr(std::string expr)
     return true;
 }
 
-void proccessTheExpr(std::string expr)
+void    operation()
+
+void proccessTheExpr(std::string &expr)
 {
-    std::stack<int> holder;
-    
+    std::stack<int> container;
+    std::string temp = expr;
+    size_t  pos;
+    std::string element;
+    int num;
+    int n1, n2;
+    for (;;)
+    {
+        pos = temp.find(" ");
+        if (pos == std::string::npos)
+        {
+            if (temp.length() == 0)
+                break;
+        }
+        else
+        {
+            element = temp.substr(0, pos);
+            temp = temp.substr(pos);
+            if (element == "+")
+            {
+                if (container.size() < 2)
+                    throw std::runtime_error("Error: The container dosen't have enough data!");
+                n1 = container.top();
+                container.pop();
+                n2 = container.top();
+                container.pop();
+            }
+            else if (element == "-")
+            {
+                if (container.size() < 2)
+                    throw std::runtime_error("Error: The container dosen't have enough data!");
+            }
+            else if (element == "*")
+            {
+                if (container.size() < 2)
+                    throw std::runtime_error("Error: The container dosen't have enough data!");
+            }
+            else if (element == "/")
+            {
+                if (container.size() < 2)
+                    throw std::runtime_error("Error: The container dosen't have enough data!");
+            }
+            else
+            {
+                num = strtod(element.c_str(), NULL);
+                container.push(num);
+            }
+        }
+        if (temp.length() == 0)
+            break;
+    }
 }
 
 void run(char *str)
