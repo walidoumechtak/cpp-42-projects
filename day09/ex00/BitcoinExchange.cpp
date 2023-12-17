6211277cb04e094cc1fc68249374f0ad68b84fe1
Walid Oumechtak <woumecht@e1r13p7.1337.ma>
2023-12-17 11:58:49 +0100

    fix some issue in ex 00

--- day09/ex00/BitcoinExchange.cpp
+++ day09/ex00/BitcoinExchange.cpp
@@ -18,6 +18,31 @@ BitcoinExchange &BitcoinExchange::operator= (const BitcoinExchange& source)
     return *this;
 }
 
+void    ltrim(std::string &source)
+{
+    int i;
+
+    i = 0;
+    while (std::isspace(source[i]))
+        i++;
+    source = source.substr(i);
+}
+
+void    rtrim(std::string &source)
+{
+    int i;
+
+    i = source.length() - 1;
+    while (i > 0)
+    {
+        if (isspace(source[i]))
+            i--;
+        else
+            break;
+    }
+    source = source.substr(0, i + 1);
+}
+
 void    BitcoinExchange::openFiles()
 {
     this->f_data.open("data.csv");
@@ -81,6 +106,8 @@ int check_value(std::string value, std::string controller)
     char *endptr;
     double val = strtod(value.data(), &endptr);
 
+    if (value.find("-") != std::string::npos)
+        return -1;
     if (endptr[0] != '\0' || endptr == value.data() || val < 0 || val > INT_MAX || (controller == "val" && val > 1000))
     {
         return (-1);
@@ -110,22 +137,35 @@ int check_date(std::string date)
     std::string day;
     int arr[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+    // check the form of the date is correct or not.!
     size_t pos = date.find("-", 1);
     if (count_element(date, "-") != 3 || date.length() != 10)
     {
         std::cout << "Error: invalid date format " << date << std::endl;
         return (-1);
     }
+    // extract each part of the date <year>, <month>, <day>.
     year = date.substr(0, pos);
     std::string temp = date.substr(pos + 1, date.length());
     pos = temp.find("-");
     month = temp.substr(0, pos);
     day = temp.substr(pos + 1, temp.length());
+
+    // converte them to int.
     int day_int = strtod(day.data(), NULL);
     int month_int = strtod(month.data(), NULL);
     int year_int = strtod(year.data(), NULL);
+
+    // check if the year is leap or not if yes add one day to the second month.
     if (month_int == 2 && year_int % 4 == 0)
         arr[1] = 29;
+
+    // here just check for error.
+    /**
+    *   if month is biger than 12.
+    *   if the entred day is bigger than the excepcted day.
+    *   ..., and some more check in check_value() function.
+    */
     if ((month_int) > 12 || check_value(year, "non") == -1 || check_value(month, "non") == -1 
         || check_value(day, "non") == -1 || arr[month_int - 1] < day_int)
     {
@@ -155,73 +195,83 @@ void BitcoinExchange::parseInput()
     std::string big_date;
     std::map<std::string, std::string>::iterator it;
 
+    // fill the map<string, string> with its value.
     fill_data_map();
+    // export the first and the last day in the database.
     it = data.end();
     small_date = data.begin()->first;
     --it;
     big_date = it->first;
+    
+    // get the first line and check it. 
     getline(this->f_input, first_line);
     if (first_line.length() <= 0 || first_line != "date | value")
     {
         std::cerr << "Error: check the form of your input file!" << std::endl;
         return;
     }
-   
+    // start the proccessing of the input file.
     while (getline(this->f_input, line))
     {
         int cpt = count_element(line, " ");
         size_t  pos = line.find("|");
 
-        if (line == "date | value" || line.length() == 0 || isOnlyWhiteSpaces(line))
+        if (line.length() == 0 || isOnlyWhiteSpaces(line))
             continue;
+        // extract the date for the input line 
         date = line.substr(0, pos - 1);
+        ltrim(date); // this function ignore the whitespaces from the left of the string.
+
         /**
          * check if | not found 
          * or there is more than one space before and after |
-         * or the compenent of the line is different from 3
+         * or the compenent of the line is different than 3
         */
         if (pos == std::string::npos || (line[pos - 2] == ' ' || line[pos + 2] == ' ') || cpt != 3)
             std::cerr << "Error: bad input => " << line << std::endl;
-        else if (date >= big_date || date <= small_date)
+        else if (date >= big_date || date <= small_date) // check the boundry of the date. 
         {
             std::cerr << "Error: please enter a date between: [ " << small_date << ", " << big_date << " ]" << std::endl;
             continue;
         }
         else
         {
+            std::string valFormInputString = (line.substr(pos + 2, line.length()));
+            rtrim(valFormInputString);
             int date_result = check_date(date);
-            int value_result = check_value(line.substr(pos + 2, line.length()), "val");
+            int value_result = check_value(valFormInputString, "val");
             if (value_result == -1)
             {
-                std::cerr << "Error: incorrect value: " << line.substr(pos + 2, line.length()) << std::endl;
+                std::cerr << "Error: incorrect value: " << valFormInputString << std::endl;
                 continue;
             }
             if (date_result == -1)
                 continue;
-            std::string dateInput = line.substr(0, pos - 1);
-            std::map<std::string, std::string>::iterator it = data.find(dateInput);
+            // searcch for the target date in the map.
+            std::map<std::string, std::string>::iterator it = data.find(date);
             if (it == data.end())
             {
-                data.insert(std::make_pair(dateInput, "0"));
-                it = data.find(dateInput);
-                --it;
+                // if not found insert it inside the map.
+                data.insert(std::make_pair(date, "0"));
+                it = data.find(date); // we just get our inserted date | value, good teeps.
+                --it; // to get the nearest.
             }
             if (it != data.end())
             {
-                double valFromData = strtod(it->second.c_str(), NULL);
-                double valFromInput = strtod(line.substr(pos + 2, line.length()).c_str(), NULL);
-
+                double valFromData = strtod(it->second.c_str(), NULL); // get the targeted value from data map (database).
+                double valFromInput = strtod((valFormInputString).c_str(), NULL); // get the value from the input file.
+                //finaly print the result.
                 std::cout << line.substr(0, pos - 1);
                 std::cout << " => ";
                 std::cout << valFromInput;
                 std::cout << " = ";
                 std::cout << valFromData * valFromInput << std::endl;
-                data.erase(++it);
+                data.erase(++it); // erase in case of the same date was found in the input file.
             }
         }
     }
 }
-// small and big date.
+
 void BitcoinExchange::run()
 {
     openFiles();
